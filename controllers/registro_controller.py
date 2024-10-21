ef09710d4b9b92b7cc60359eea80ff53ececdae2
Daniel Frausto <oscar.daniel@frausto.mx>
2024-10-21 01:39:23 -0600

    fixed: render_template undefined

--- controllers/registro_controller.py
+++ controllers/registro_controller.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, request, jsonify
+from flask import Blueprint, request, jsonify, render_template
 from marshmallow import ValidationError
 from models.registro import Registro
 from schemas.schemas import RegistroSchema
